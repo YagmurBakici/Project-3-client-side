60e67cb35f2313d6f02fa0b3a0ef3ddaff0e957a
Thanh Nguyen <Thanh.ngu94@gmail.com>
2019-05-09 10:02:30 +0200

    delete fake comment

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem odit
-        quasi voluptatem consequuntur eos alias iste id nihil dolor quod neque
-        nisi quia, similique veritatis non magnam nostrum necessitatibus rem
-        optio ea eius. Nam nostrum sit voluptate eum quas nemo ullam recusandae,
-        ad, blanditiis tempora eaque iusto, deserunt obcaecati tenetur. Iure
-        reiciendis, architecto nam nisi possimus dignissimos molestias quod
-        tempora.
+        XPT is designed to provide you with all the resources you need to ease you into life in a new city. Our aim is to make things easier for you as you plan your move and settle into your new life in.XPT is there to make you feel quickly at home wherever you are in the world.
+        Easier, faster, cheaper.
+        Step 1: Type your futur address
+        Step 2:Choose the services that you need
+        Step 3: Sign Up
+        Step 4: Drag Drop your files
+        Step 5:Now you can go and relax, XPT will handle all your formalities
       </p>
       <SearchBox />
     </div>
