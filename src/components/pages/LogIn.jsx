929ce17c4477cc04b1d103554d4e83824276b766
Thanh Nguyen <Thanh.ngu94@gmail.com>
2019-05-10 12:06:03 +0200

    dev du form login en cours, ajout du state, evt listeners, etc

--- src/components/pages/LogIn.jsx
+++ src/components/pages/LogIn.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
 export default class LogIn extends Component {
+
+  state = {
+    email: "",
+    password: ""
+  }
+
+  handleInput = (evt) => {
+    // console.log(evt.target.name, evt.target.value)
+    this.setState({[evt.target.name]: evt.target.value});
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(evt);
+  }
+
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <h1>LOG IN PAGE</h1>
-      </div>
+        <input name="email" onChange={this.handleInput} />
+        <input name="password" onChange={this.handleInput} />
+        <button>ok</button>
+      </form>
     );
   }
 }
