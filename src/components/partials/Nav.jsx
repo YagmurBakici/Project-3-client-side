8f16e6b570f228a69ba1c20461420ef9fc3ff23a
Thanh Nguyen <Thanh.ngu94@gmail.com>
2019-05-14 10:02:54 +0200

    changes on nav main, conflicts ahead

--- src/components/partials/Nav.jsx
+++ src/components/partials/Nav.jsx
@@ -1,49 +1,127 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React,{Component} from "react";
+import { NavLink, Link} from "react-router-dom";
+import  AuthService from "./../auth/ajax";
+export default class Nav extends Component{
 
-export default function Nav() {
-  return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <NavLink exact to="/">
-          <img src="./../xpt.png" alt="logo" width="100rem" height="600rem" />
-        </NavLink>
-        <a
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-        </a>
-      </div>
+  constructor(props){
+    super(props)
+    this.state= {loggedInUser:null}
+    this.service = new AuthService()
+  }
+  
+  componentWillReceiveProps(nextProps){
+    console.log("iciiiiii", nextProps)
+    this.setState({...this.state, loggedInUser: nextProps["userInSession"]  });
+  }
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <NavLink to="/" className="navbar-item">
-            Home
-          </NavLink>
-          <NavLink to="/contact" className="navbar-item">
-            Contact
-          </NavLink>
-        </div>
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <NavLink className="button is-primary" to="/SignUp">
-                Sign Up
+  // logoutUser = () => {
+  //   this.service.logout()
+  //     .then(()=>{
+  //       this.setState({ loggedInUser: null  });
+  //       // this.props.getUser(null)
+  //     })
+  // }
+
+  render(){
+
+    ////////////////////////////////////////////////
+    /////////////IF LOGGED IN///////////////////////
+    ////////////////////////////////////////////////
+    if(this.state.loggedInUser){
+      return (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <NavLink exact to="/">
+              <img src="./../xpt.png" alt="logo" width="100rem" height="600rem" />
+            </NavLink>
+            <a
+              role="button"
+              className="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <NavLink to="/" className="navbar-item">
+                Home
               </NavLink>
-
-              <NavLink to="/login" className="button is-light">
-                Log in
+              <NavLink to="/contact" className="navbar-item">
+                Contact
               </NavLink>
             </div>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <NavLink className="button is-primary" to="/Profile">
+                    Profile
+                  </NavLink>
+
+                  <Link to="/" className="button is-light">
+                  <button onClick={this.props.logoutFromParent}>Logout</button>
+                    {/* <button onClick={()=> this.logoutUser()}>Logout</button> */}
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </nav>
-  );
+        </nav>
+      ); 
+
+    ////////////////////////////////////////////////
+    /////////////IF NOT LOGGED//////////////////////
+    ////////////////////////////////////////////////
+    } else{
+      return(
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <NavLink exact to="/">
+              <img src="./../xpt.png" alt="logo" width="100rem" height="600rem" />
+            </NavLink>
+            <a
+              role="button"
+              className="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <NavLink to="/" className="navbar-item">
+                Home
+              </NavLink>
+              <NavLink to="/contact" className="navbar-item">
+                Contact
+              </NavLink>
+            </div>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <NavLink className="button is-primary" to="/SignUp">
+                    Sign Up
+                  </NavLink>
+
+                  <NavLink to="/login" className="button is-light">
+                    Log in
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      )
+    }
+  }
 }
