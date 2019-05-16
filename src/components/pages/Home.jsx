46fb106ff28744bc59cafea22e88cee7529c72f5
Yagmur <bakiciyagmur@gmail.com>
2019-05-16 17:09:01 +0200

    home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -23,18 +23,25 @@ export default class Home extends Component {
             is there to make you feel quickly at home wherever you are in the
             world.
             <br /> Easier, faster, cheaper.
-            <p>
+            <div className="steps">
               <strong>Step 1:</strong> Fill the form hereunder with your basic
               information
               <br /> <strong>Step 2:</strong> Then choose the services that you
               need
-              <br /> <strong>Step 3: </strong> Sign Up
-              <br /> <strong>Step 4: </strong> Drag and drop required files
-              <br /> <strong>Step 5: </strong> Now you can go and relax, we will
-              handle all your formalities
-            </p>
+              <br /> <strong className="step">Step 3: </strong> Sign Up
+              <br /> <strong className="step">Step 4: </strong> Drag and drop
+              required files
+              <br /> <strong className="step">Step 5: </strong> Now you can go
+              and relax, we will handle all your formalities
+            </div>
           </div>
-          <img src="image.png" alt="logol" className="image" />
+          <img
+            src="image.png"
+            alt="logol"
+            className="image"
+            width="400"
+            height="200"
+          />
         </div>
 
         {/* <Boxes /> */}
