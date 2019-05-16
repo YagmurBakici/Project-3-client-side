e9f1a09ba45a230deeb26f705b808de0419ec65a
Yagmur <bakiciyagmur@gmail.com>
2019-05-16 19:16:54 +0200

    home pge

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -1,11 +1,11 @@
-// import React, { Component } from "react";
-// // import {Link} from "react-router-dom"
+import React, { Component } from "react";
+// import {Link} from "react-router-dom"
 
-// export default class Admin extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     return <section id="admin_panel">admin here</section>;
-//   }
-// }
+export default class Admin extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <section id="admin_panel">admin here</section>;
+  }
+}
